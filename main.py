0c93fd18d85547e248eae86a2869477849fe2433
mateus.torres <mateus-t-torres@pm.me>
2024-08-04 11:26:00 -0300

    improve variable names and formatting

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
 file = "books/frankenstein.txt"
 
 with open(file) as f:
-    file_contents = f.read()
-    words = file_contents.split()
-    characters = {}
-    for w in words:
-        for raw_char in w:
-            if raw_char.isalpha():
-                c = raw_char.lower()
-                if c not in characters:
-                    characters[c] = 0
-                characters[c] = characters[c] + 1
+    raw_file = f.read()
+    all_words = raw_file.split()
 
-    lst = sorted(
-        ((x, characters[x]) for x in characters), key=lambda x: x[1], reverse=True
+    characters = {}
+    for word in all_words:
+        for char in word:
+            if char.isalpha():
+                letter = char.lower()
+                if letter not in characters:
+                    characters[letter] = 0
+                characters[letter] += 1
+
+    ordered_letter_count_list = sorted(
+        ((letter, characters[letter]) for letter in characters),
+        key=lambda letter_count_tuple: letter_count_tuple[1],
+        reverse=True,
     )
 
     print(f"--- Bookbot Report of {file} ---")
-    print(f"{len(words)} total words were found in the file")
+    print(f"{len(all_words)} total words were found in the file")
     print()
 
-    for tp in lst:
-        print(f'The "{tp[0]}" character was found {tp[1]} times')
+    for item in ordered_letter_count_list:
+        print(f'The "{item[0]}" character was found {item[1]} times')
 
     print()
     print("--- End Report ---")
